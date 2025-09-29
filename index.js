110a7636a5e85dc6f09b88e176fd7684de63bf19
afsheenaslam <afsheenaslamit@gmail.com>
2025-09-29 11:55:20 +0500

    cors issue solved

--- index.js
+++ index.js
@@ -10,16 +10,36 @@ import { error } from "console";
 
 const app = express();
 
-app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://full-stack-todo-app-deployed.netlify.app",
+];
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://full-stack-task-manager-app.netlify.app",
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
+app.use(express.json());
+
+app.options("*", cors());
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://full-stack-todo-app-deployed.netlify.app",
+//     ],
+//     credentials: true,
+//   })
+// );
 app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
