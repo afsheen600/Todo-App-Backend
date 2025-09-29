e7ed7421ebe5b3683cea93b02af1906a763e900d
afsheenaslam <afsheenaslamit@gmail.com>
2025-09-29 13:39:20 +0500

    cors issue

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use(cors());
 app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
