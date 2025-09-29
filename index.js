1c1b718835e183d8ac922ebce9ff4cb58d2190cd
afsheenaslam <afsheenaslamit@gmail.com>
2025-09-29 10:51:59 +0500

    cors issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import { ObjectId } from "mongodb";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
+
 import { error } from "console";
 
 const app = express();
@@ -12,7 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://full-stack-task-manager-app.netlify.app",
+    ],
     credentials: true,
   })
 );
